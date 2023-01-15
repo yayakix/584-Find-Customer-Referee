33cb27942fb76bea4c4e917b62ec6c3b5326e2ea
Iyana <87146606+yayakix@users.noreply.github.com>
2023-01-14 23:38:30 -0500

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,5 @@
+# SELECT name FROM Customer WHERE referee_id <> 2 OR referee_id IS NULL;
+# Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+
+# Return the result table in any order.
+
